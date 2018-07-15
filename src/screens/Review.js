9e7940ffed99007ae38adce32fea95dc2c2da28c
Oleksandr <afanasievov@gmail.com>
2018-07-15 14:46:00 +0300

    Add Styling to NavBar

--- src/screens/Review.js
+++ src/screens/Review.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform } from 'react-native';
 
 import { Button } from 'react-native-elements';
 
@@ -10,8 +10,13 @@ class Review extends Component {
       <Button
         title="Settings"
         onPress={() => navigation.navigation.navigate('settings')}
+        backgroundColor="rgba(0,0,0,0)"
+        color="rgba(0, 122, 255, 1)"
       />
     ),
+    style: {
+      marginTop: Platform.OS === 'android' ? 24 : 0,
+    },
   })
 
   render() {
