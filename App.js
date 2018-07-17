afba9044b2cf986cb7ee37de9d95823f14749cfe
Oleksandr <afanasievov@gmail.com>
2018-07-17 23:41:14 +0300

    Setup redux

--- App.js
+++ App.js
@@ -1,5 +1,11 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 
+import store from 'store';
 import MainNavigator from './src/hoc/Navigator';
 
-export default () => <MainNavigator />;
+export default () => (
+  <Provider store={store}>
+    <MainNavigator />
+  </Provider>
+);
