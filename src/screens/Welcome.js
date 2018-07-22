63a5c9ef0d7ad97042e7208c37be2af78632f303
Oleksandr <afanasievov@gmail.com>
2018-07-22 15:29:09 +0300

    Implement AppLoading (Expo)

--- src/screens/Welcome.js
+++ src/screens/Welcome.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AppLoading } from 'expo';
+import { AsyncStorage } from 'react-native';
+import _ from 'lodash';
 
 import Slides from 'components/Slides/Slides';
 import * as SCREENS from 'constants/screens';
+import * as LS from 'constants/localStorage';
 
 const SLIDE_DATA = [
   { text: 'Welcome to JobApp', color: '#03A9F4' },
@@ -9,12 +13,30 @@ const SLIDE_DATA = [
   { text: 'Set your location, then swipe away', color: '#03A9F4' },
 ];
 
-const welcome = ({ navigation }) => {
-  const onSlidesComplete = () => {
-    navigation.navigate(SCREENS.AUTH);
+class Welcome extends Component {
+  state = { token: null };
+
+  async componentWillMount() {
+    const token = await AsyncStorage.getItem(LS.JOBS_FACEBOOK_TOKEN);
+
+    if (token) {
+      this.props.navigation.navigate(SCREENS.MAP);
+      this.setState({ token });
+    } else {
+      this.setState({ token: false });
+    }
+  }
+
+  onSlidesComplete = () => {
+    this.props.navigation.navigate(SCREENS.AUTH);
   };
 
-  return <Slides data={SLIDE_DATA} onComplete={onSlidesComplete} />;
-};
+  render() {
+    if (_.siNull(this.state.token)) {
+      return <AppLoading />;
+    }
+    return <Slides data={SLIDE_DATA} onComplete={this.onSlidesComplete} />;
+  }
+}
 
-export default welcome;
+export default Welcome;
